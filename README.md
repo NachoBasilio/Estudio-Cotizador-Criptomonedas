b6c5e696fc08044a8bbf170e575d0d8903fa2ae6
IgnaDev <arcortoon@gmail.com>
2023-02-27 19:32:44 -0300

    learn: tome mis primeros apuntes sobre syled components y tambien cree mi primer componente de este estilo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cotizador-criptomonedas
+[notion stiled componentes](https://political-thing-0ef.notion.site/Styled-Components-a5e38cb29d494d38b718f5154c136197)
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
 
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff
+`
 
 
 function App() {
-
   return (
-    <div className="App">
-      Hola
-    </div>
+    <Heading className="App">
+      Desde App
+    </Heading>
   )
 }
 
