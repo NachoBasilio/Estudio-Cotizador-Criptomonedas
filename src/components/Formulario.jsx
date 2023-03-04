313ee682692b100dfae04173cb399046c0922ed8
IgnaDev <arcortoon@gmail.com>
2023-03-04 20:28:11 -0300

    feat: ya esta formateando el array y llamando a esos valores desde el hook que creamos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useEffect,useState} from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
@@ -22,22 +22,36 @@ const InputSubmit = styled.input`
 `
 
 export default function Formulario() {
+    const [criptos, setCriptos] = useState([]);
+
     const [SelectMonedas, moneda] = useSelectMonedas("Elije tu moneda", monedas)
+    const [SelectCriptos, cripto] = useSelectMonedas("Elije tu criptomoneda", criptos)
 
     useEffect(()=>{
       const consultarAPI = async ()=>{
-        const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
+        const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
-        console.log(resultado)
+
+        const arrayCriptos = resultado.Data.map((cripto)=>{
+          const objeto = {
+            id:cripto.CoinInfo.Name,
+            nombre: cripto.CoinInfo.FullName
+          }
+          return objeto
+        })
+        setCriptos(arrayCriptos)
       }
       consultarAPI()
+      
     },[])
+
   return (
     <form>
 
         <SelectMonedas/>
-        
+        <SelectCriptos/>
+
         <InputSubmit type="submit" value="Cotizar"></InputSubmit>
     </form>
   )
